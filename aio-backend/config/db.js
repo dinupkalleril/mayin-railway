7017d3c29eda26ca54441ed165199585087d3d6c
Dinup Kalleril <61725213+dinupkalleril@users.noreply.github.com>
2026-02-10 19:28:24 +0530

    PostgreSQL user not set error fixing

--- aio-backend/config/db.js
+++ aio-backend/config/db.js
@@ -7,7 +7,5 @@ if (!process.env.DATABASE_URL) {
 
 export const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
-  ssl: process.env.NODE_ENV === "production"
-    ? { rejectUnauthorized: false }
-    : false,
+  ssl: { rejectUnauthorized: false }, // ALWAYS enable SSL on Railway
 });
